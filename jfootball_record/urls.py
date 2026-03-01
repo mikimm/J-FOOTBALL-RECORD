f661c68bac8b03b76bf886ea044f94788e4d1f0e
mikihiromakino <16id014mmiki@gmail.com>
2026-03-01 10:19:00 +0900

    URLディスパッチャー修正

--- jfootball_record/urls.py
+++ jfootball_record/urls.py
@@ -4,8 +4,8 @@ from jfootball_record.views.team_views import TeamListView
 from jfootball_record.views.match_records_views import MatchRecordsView
 from jfootball_record.views.comments_views import CommentsView
 urlpatterns = [
-    path('create/user', UserView.as_view()),
-    path('create/match_records', MatchRecordsView.as_view()),
-    path('create/comments/<int:match_records_id>', CommentsView.as_view()),
+    path('user', UserView.as_view()),
+    path('match_records', MatchRecordsView.as_view()),
+    path('comments/<int:match_records_id>', CommentsView.as_view()),
     path('teams/<int:league_id>', TeamListView.as_view()),
 ]
