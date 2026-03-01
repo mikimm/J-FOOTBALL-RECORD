39e3936997943fe68c71a6adae4482ac24023d3b
mikihiromakino <16id014mmiki@gmail.com>
2026-03-01 15:42:18 +0900

    queryset追加

--- jfootball_record/views/match_records_views.py
+++ jfootball_record/views/match_records_views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
+from jfootball_record.model_definition.match_records_models import MatchRecords
 from jfootball_record.serializer.match_records_serializer import MatchRecordsSerializer
 # Create your views here.
 class MatchRecordsView(generics.CreateAPIView):
     serializer_class = MatchRecordsSerializer
-
+    queryset = MatchRecords.objects.all()
+        
     def perform_create(self, serializer):
         serializer.save(created_by_id=1)
