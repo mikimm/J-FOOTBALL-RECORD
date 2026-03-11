6ec26403e41730fb7a1404de1c48a0d0b1b66d2a
mikihiromakino <16id014mmiki@gmail.com>
2026-03-11 21:47:09 +0900

    いいね登録機能view_setに移行

--- jfootball_record/serializer/nice_serializer.py
+++ jfootball_record/serializer/nice_serializer.py
@@ -1,9 +1,31 @@
+from django.forms import IntegerField
 from rest_framework import serializers
 
+from jfootball_record.model_definition.match_records_models import MatchRecords
 from jfootball_record.model_definition.nice_models import Nice
+from jfootball_record.model_definition.users_models import Users
 
 # Comments用シリアライザー
 class NiceSerializer(serializers.ModelSerializer):
+    record_id =serializers.IntegerField()
+    post_by_id =serializers.IntegerField()
     class Meta:
         model=Nice
-        fields =  ['record', 'post_by']
+        fields =  ['record', 'post_by','record_id','post_by_id']
+        read_only_fields = ['record', 'post_by']
+    def validate(self, data):
+            """
+            Check Record and User
+            """
+            if not Users.objects.filter(id=data["post_by_id"]).exists():
+                raise serializers.ValidationError("user not found")
+            elif not MatchRecords.objects.filter(id=data["record_id"]).exists():
+                raise serializers.ValidationError("record not found")
+            
+            u=Users.objects.get(id=data["post_by_id"])
+            m=MatchRecords.objects.get(id=data["record_id"])
+            
+            if u.id == m.created_by_id:
+                raise serializers.ValidationError("not permissioned")
+            
+            
--- jfootball_record/urls.py
+++ jfootball_record/urls.py
@@ -1,3 +1,4 @@
+
 from django.urls import path
 from jfootball_record.adaptor.team_adaptor import Team_Adaptor
 from jfootball_record.usecase.team_usecase import Team_Usecase
@@ -6,16 +7,16 @@ from jfootball_record.views.team_views import TeamDetailView, TeamListView
 from jfootball_record.views.match_records_views import MatchRecordsView
 from jfootball_record.views.comments_views import CommentsView
 from jfootball_record.views.nice_views import NiceView
+from rest_framework.routers import DefaultRouter
 
 au=Team_Adaptor()
 tu = Team_Usecase(au)
-
-
+router=DefaultRouter()
+router.register('nice',NiceView)
 urlpatterns = [
     path('user', UserView.as_view()),
     path('match_records', MatchRecordsView.as_view()),
     path('comments/<int:match_records_id>', CommentsView.as_view()),
-    path('nice/<int:match_records_id>', NiceView.as_view()),
     path('teams/<int:league_id>', TeamListView.as_view()),
     path('teams/detail/<int:team_id>', TeamDetailView.as_view(usecase=tu)),
-]
+]+router.urls
