1ea3ae4668040c32dd3b8425b5b09747afd2c540
mikihiromakino <16id014mmiki@gmail.com>
2026-03-14 19:46:06 +0900

    共通エラーハンドラー追加

--- jfootball_record/exception/exception_handler.py
+++ jfootball_record/exception/exception_handler.py
@@ -1,11 +1,11 @@
 
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
 from jfootball_record.exception.exceptions import ExternalAPIError, NotFoundError
 from django.core.exceptions import ObjectDoesNotExist
 
 def hundle_exception(exc:Exception) -> JsonResponse:
 
-    if isinstance(exc, NotFoundError) or isinstance(exc, ObjectDoesNotExist):
+    if isinstance(exc, NotFoundError) or isinstance(exc, ObjectDoesNotExist) or isinstance(exc, Http404):
         return JsonResponse({"error": str(exc)}, status=404)
 
     elif isinstance(exc, ExternalAPIError):
