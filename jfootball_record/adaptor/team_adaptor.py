0b8bc8ec066283fbea1b22f1c261da7d9c3dea46
mikihiromakino <16id014mmiki@gmail.com>
2026-03-28 18:13:59 +0900

    バグ修正

--- jfootball_record/adaptor/team_adaptor.py
+++ jfootball_record/adaptor/team_adaptor.py
@@ -1,14 +1,14 @@
 from typing import Any
 import requests
-
+from backend.settings import SEASON, X_API_SPORTS_KEY
 class Team_Adaptor():
     def call(self,**kwargs) -> Any:
         team_id=kwargs["team_id"]
         parameter = {
         "id": {team_id}
         }
-        headers = {"x-apisports-key": ""}
-        response = requests.get("https://v3.foqotball.api-sports.io/teams", params=parameter,headers=headers)
+        headers = {"x-apisports-key": X_API_SPORTS_KEY}
+        response = requests.get("https://v3.football.api-sports.io/teams", params=parameter,headers=headers)
         if response.status_code !=200:
             raise Exception(response.json())  
         return_data = {"status": response.status_code, "data": response.json()}     
